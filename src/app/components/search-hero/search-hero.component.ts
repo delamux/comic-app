32a2358d1656c2ef180c567a0fff70fb1a5d4d78
delAmux <luisdelamo@gmail.com>
2019-06-28 22:27:04 +0100

    ref:#master adding alert

--- src/app/components/search-hero/search-hero.component.ts
+++ src/app/components/search-hero/search-hero.component.ts
@@ -4,10 +4,11 @@ import { HeroesService, Hero } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-search-hero',
-  templateUrl: '../heroes/heroes.component.html'
+  templateUrl: './search-hero.component.html'
 })
 export class SearchHeroComponent implements OnInit {
   heroes: Hero[] = [];
+  term: string;
   constructor(
     private activatedRoute: ActivatedRoute,
     private heroesService: HeroesService
@@ -16,6 +17,7 @@ export class SearchHeroComponent implements OnInit {
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       this.heroes = this.heroesService.findHeroes(params.term);
+      this.term = params.term;
     });
   }
 
